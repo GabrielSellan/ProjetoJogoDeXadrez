d289ce27b1df8f9a745082eab9a8fa831d0ae279
GabrielSellan <gabrielsellan2002@gmail.com>
2023-07-16 19:09:30 -0300

    Projeto xadrez criado

---
+++ Xadrez-game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XadrezGame // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
