4b3fb2d395a8d4dbdd5adfd63e7f41843fabfb0e
GabrielSellan <gabrielsellan2002@gmail.com>
2023-07-22 23:58:05 -0300

    Começando a colocar peças no tabuleiro

--- Xadrez-game/Program.cs
+++ Xadrez-game/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using tabuleiro;
 using Xadrez_game;
+using xadrez;
 
 namespace xadrez_console // Note: actual namespace depends on the project name.
 {
@@ -10,9 +11,14 @@ namespace xadrez_console // Note: actual namespace depends on the project name.
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
             Tela.ImprimirTabuleiro(tab);
-            
+
             Console.ReadLine();
         }
     }
 }
+
